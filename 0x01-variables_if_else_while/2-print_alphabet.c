a858e5a3af5f12cee519c02b306c7bc7a756bcf0
Karoogh <unityokus@yahoo.com>
2023-03-18 08:48:54 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This program prints the alphabet
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
