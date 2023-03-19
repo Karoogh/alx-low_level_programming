355165dff5ea84c434d7ae85f3a14dd3b26b3605
Karoogh <unityokus@yahoo.com>
2023-03-19 16:01:47 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,10 +5,17 @@
  */
 int main(void)
 {
-	int i;
+	int d;
 
-	for (i = 48; i < 58; i++)
-		putchar(i);
+	for (d = '0'; d < '9'; d++)
+	{
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
