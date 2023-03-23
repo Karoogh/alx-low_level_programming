1bd7392c06d9f84155f96811cf197b5e8ab65089
Karoogh <unityokus@yahoo.com>
2023-03-23 02:13:46 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Function that tests if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive or negative(i);
+	return (0);
+}
